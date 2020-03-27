3f3a7047973157b0b3849fd044a48b0975d39a1a
Patrick Pegoretti <ppegoretti@hotmail.com>
2020-03-27 19:20:38 -0300

    Mobile da aplicação + Reset do banco

---
+++ mobile/App.js
@@ -0,0 +1,12 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import React from 'react';
+
+import Routes from './src/Routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
